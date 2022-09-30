b48ec0f59725fc246a9aa3dac2568ab8f7031ae3
Leandro-Molinari <leandro.molinari@brxretail.com>
2022-09-30 11:44:11 -0300

    Inicio da solução de cliente

--- src/br/com/comex/modelo/Cliente.java
+++ src/br/com/comex/modelo/Cliente.java
@@ -4,7 +4,8 @@ package br.com.comex.modelo;
 
 public class Cliente {
 	
-	private static int id  = 1;
+	private static int contador  = 1;
+	private int id;
 	private String nome;
 	private String cpf;
 	private String telefone;
@@ -18,7 +19,38 @@ public class Cliente {
 	public Cliente (int id, String nome, String cpf, String telefone, String rua, String numero, String complemento,
 	 String bairro, String cidade, String estado) {
 		
-		id = id++;
+		if (id != contador) {
+			throw new IllegalArgumentException("Id inválido, o próximo Id disponível é: " + contador);
+		}
+		
+		if (nome.length() <= 5) {
+			new IllegalArgumentException("Nome deve ter mais que 5 letras");
+		}
+		
+		if (cpf.length() < 11 || cpf.length() > 14) {
+			new IllegalArgumentException("CPF inválido, deve ter entre 11 e 14 números");
+		}
+		
+		if(telefone.length() < 11 || telefone.length() > 16) {
+			new IllegalArgumentException("Telefone inválido, deve ter entre 11 e 16 caracteres");
+		}
+		
+		if(rua.length() <= 5) {
+			throw new IllegalArgumentException("Nome da rua inválido, deve ter mais de 5 caracteres");
+		}
+		
+		if(numero.length() <= 1) {
+			throw new IllegalArgumentException("Informar número correto (Minimo 2 números ou S/N");
+		}
+		
+		if(bairro.length() <= 1) {
+			throw new IllegalArgumentException("Informar nome do bairro corretetamente");
+		}
+		
+		if(cidade.length() <= 1) {
+			throw new IllegalArgumentException("Nome da Cidade bairro corretetamente");
+		}
+		
 		
 		
 	}
---
+++ src/comex/ItemPedido.java
@@ -0,0 +1,92 @@
+package comex;
+
+public class ItemPedido {
+	
+	private static int contador = 1;
+	private int id;
+	private Produto precoUnitario;
+	private int quantidade;
+	private Produto produto;
+	private Pedido pedido;
+	private double desconto;
+	private String tipoDeDesconto;
+		
+	public ItemPedido (int quantidade, Produto produto, 
+								Pedido pedido, String tipoDeDesconto) {
+		
+		this.id = contador;
+//		this.precoUnitario = precoUnitario;
+		this.quantidade = quantidade;
+		this.produto = produto;
+		this.pedido = pedido;
+		this.tipoDeDesconto = tipoDeDesconto;
+		
+		contador++;
+	}
+	
+	public double PrecoTotalSemDesconto() {
+		double precoTotal = Produto.getPrecoUnitario();
+		return precoTotal;
+	}
+	
+	public double calculaDesconto() {
+	
+	if(tipoDeDesconto != "NENHUM" && tipoDeDesconto != "PROMOÇÃO") {
+		System.out.println("Tipo de desconto inválido");
+	} else {
+			if(tipoDeDesconto == "NENHUM" && quantidade <= 10) {
+				desconto = 0.0;
+			}else {
+					if (tipoDeDesconto == "NENHUM" && quantidade > 10) {
+						desconto = 0.1;
+					}else {
+						if(tipoDeDesconto == "PROMOÇÃO") {
+							desconto = 0.2;
+						}
+						
+					}
+			}		
+	}
+	return desconto;
+}	
+	
+	
+	
+	
+	public double precoTotalComDesconto(){
+		
+		double PrecoTotalSemDesconto = PrecoTotalSemDesconto() - (PrecoTotalSemDesconto() * desconto);
+		return PrecoTotalSemDesconto;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public double getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public String getTipoDeDesconto() {
+		return tipoDeDesconto;
+	}
+	
+	
+	
+}
