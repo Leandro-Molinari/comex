e44b5c18adea6825e0ad4917303b50059bca3476
Leandro-Molinari <leandro.molinari@brxretail.com>
2022-10-16 16:22:35 -0300

    Correção e inclusão da Classe Produto_Isento

--- src/br/com/comex/main/MainInsercaoProduto.java
+++ src/br/com/comex/main/MainInsercaoProduto.java
@@ -2,6 +2,7 @@ package br.com.comex.main;
 
 import br.com.comex.modelo.Produto;
 import br.com.comex.modelo.Produto.tipo;
+import br.com.comex.modelo.ProdutoIsento;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -25,11 +26,11 @@ public class MainInsercaoProduto {
 																("INSERT INTO comex.produto (nome, descricao, preco_unitario,"
 																		+ " quantidade_estoque, categoria_id, tipo) VALUES (?, ?, ?, ?, ?, ?)", retornaColuna)) {
 			 
-							adicionaVariavel(new Produto("Notebook Samsung", "Tela 14'', Processador i5 9ª geração, 32GB RAM, SSD 1GB", 13523.0, 1, 8,tipo.NAO_ISENTO), statement);
+							adicionaVariavel(new Produto("Notebook Samsung", "Tela 14'', Processador i5 9ª geração, 32GB RAM, SSD 1GB", 14523.0, 1, 8,tipo.NAO_ISENTO), statement);
 			
-							adicionaVariavel(new Produto("Clean Architecture", "Autor:Robert Martin", 102.90, 2, 10, tipo.ISENTO), statement);
+							adicionaVariavel(new ProdutoIsento("Clean Architecture", "Autor:Robert Martin", 104.90, 2, 10, tipo.ISENTO), statement);
 			
-							adicionaVariavel(new Produto("Monitor Dell 27''", "Ultra HD, 16:9, 2 HDMI 2.0, Bivolt", 1889.00, 3, 8, tipo.NAO_ISENTO), statement);
+							adicionaVariavel(new Produto("Monitor Dell 27''", "Ultra HD, 16:9, 2 HDMI 2.0, Bivolt", 1884.00, 3, 8, tipo.NAO_ISENTO), statement);
 	
 							System.out.println("Dados inseridos com sucesso, conexão encerrada.");
 							connection.commit();
@@ -57,7 +58,25 @@ public class MainInsercaoProduto {
 			System.out.println(" " + result.getLong(1) + " ");
 			
 		}
-	
+	}
+		private static void adicionaVariavel(ProdutoIsento produtoIsento, PreparedStatement statement) throws SQLException {
+			statement.setString(1, produtoIsento.getNome());
+			statement.setString(2, produtoIsento.getDescricao());
+			statement.setDouble(3, produtoIsento.getPreco_Unitario());
+			statement.setInt(4, produtoIsento.getQuantidade_Estoque());
+			statement.setInt(5, produtoIsento.getCategoria_id());
+			statement.setString(6, produtoIsento.getTipo().name());
+			statement.execute();
+			
+			ResultSet result = statement.getGeneratedKeys();
+		
+			while(result.next()) {
+				System.out.println(" " + result.getLong(1) + " ");
+				
+			}	
+		
+		
+		
 	}
 	
 }
--- src/br/com/comex/modelo/ProdutoIsento.java
+++ src/br/com/comex/modelo/ProdutoIsento.java
@@ -1,59 +1,31 @@
-//package br.com.comex.modelo;
-//
-//
-//
-//public class ProdutoIsento extends Produto {}
-//	
-//
-//	
-//	public ProdutoIsento(int id, String nome, String descricao, double precoUnitario, int quantidadeEmEstoque, Categoria categoria) {
-//		super(id, nome, descricao, precoUnitario, quantidadeEmEstoque, categoria);
-//		
-//		
-//		if (nome.length() <= 5) {
-//			throw new ComexException ("Nome do produto deve ter no mínimo 6 caracteres");
-//		}
-//		
-//		if (precoUnitario <= 0) {
-//			throw new ComexException ("Preço não pode ser menor ou igual a zero");
-//		}
-//		
-//		if (quantidadeEmEstoque <= 0) {
-//			throw new ComexException ("Quantidade não pode estar zerada");
-//		}
-//		
-//		if (categoria == null) {
-//			throw new ComexException ("Deve informar categoria");
-//		}
-//		
-//	}
-//
-//	public ProdutoIsento (String nome, String descricao, double precoUnitario, int quantidadeEmEstoque, Categoria categoria){
-//		super(nome, descricao, precoUnitario, quantidadeEmEstoque, categoria);
-//		
-//				if (nome.length() <= 5) {
-//					throw new ComexException ("Nome do produto deve ter no mínimo 6 caracteres");
-//				}
-//				
-//				if (precoUnitario <= 0) {
-//					throw new ComexException ("Preço não pode ser menor ou igual a zero");
-//				}
-//				
-//				if (quantidadeEmEstoque <= 0) {
-//					throw new ComexException ("Quantidade não pode estar zerada");
-//				}
-//				
-//				if (categoria == null) {
-//					throw new ComexException ("Deve informar categoria");
-//				}
-//			
-//			}
-//	
-//	public double imposto(double imposto) {
-//		imposto = 0;
-//		return imposto;
-//	}
-//	
-//	
-//	
-//}
+package br.com.comex.modelo;
+
+public class ProdutoIsento extends Produto {
+	
+
+	
+	public ProdutoIsento(String nome, String descricao, double preco_Unitario, int quantidade_Estoque, int categoria_id, tipo tipo) {
+		super(nome, descricao, preco_Unitario, quantidade_Estoque, categoria_id, tipo);
+		
+				
+		if (nome.length() <= 5) {
+			throw new ComexException ("Nome do produto deve ter no mínimo 6 caracteres");
+		}
+		
+		if (preco_Unitario <= 0) {
+			throw new ComexException ("Preço não pode ser menor ou igual a zero");
+		}
+		
+		if (quantidade_Estoque <= 0) {
+			throw new ComexException ("Quantidade não pode estar zerada");
+		}
+	}	
+
+	public double imposto(double imposto) {
+		imposto = 0;
+		return imposto;
+	}
+	
+	
+	
+}
